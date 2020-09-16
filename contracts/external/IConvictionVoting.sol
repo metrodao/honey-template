48a451a0b8b1be33477fbb7c03c1bbb91d0d7655
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-09-16 12:46:46 -0300

    Update Conviction Voting Interface

--- contracts/external/IConvictionVoting.sol
+++ contracts/external/IConvictionVoting.sol
@@ -7,16 +7,13 @@ contract IConvictionVoting {
 
     bytes32 constant public CREATE_PROPOSALS_ROLE = keccak256("CREATE_PROPOSALS_ROLE");
 
-    function initialize(MiniMeToken _stakeToken, Vault _vault, address _requestToken) public;
-
-    function initialize(MiniMeToken _stakeToken, Vault _vault, address _requestToken, uint256 _decay, uint256 _maxRatio);
-
     function initialize(
         MiniMeToken _stakeToken,
         Vault _vault,
         address _requestToken,
         uint256 _decay,
         uint256 _maxRatio,
-        uint256 _weight
+        uint256 _weight,
+        uint256 __minThresholdStakePercentage
     ) public;
 }
